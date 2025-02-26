fc0fdb5dcffe6c47d4421660a6f8610a26caa2bc
Ricardo Pereira <patresio@gmail.com>
2025-02-26 16:01:23 -0300

    implementando cors

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import 'dotenv/config'
 import express, { Express } from 'express'
 import { runMongo } from './database/mongo-cnnct'
@@ -8,6 +9,7 @@ const PORT: string = process.env.PORT || '3000'
 
 const app: Express = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/', routerIndex)
